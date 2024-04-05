8d477ea29584e619d50e5f5cf5b633685b5a950d
uotlaf <git@uotlaf.com>
2024-04-05 16:11:00 -0300

    Use ArrayString in GS
    - StudentList.students is now StudentList.0

--- src/student.rs
+++ src/student.rs
@@ -1,81 +1,86 @@
 use std::fmt;
 use std::fmt::{Formatter};
+use arraystring::{ArrayString, typenum::U30};
 
-/// Generic structure. In this case, Student
+type Name = ArrayString<U30>;
+
+/// Generic structure chosen for this TAD
 pub struct Student {
-    name: String,
+    name: Name, // Size for each name is fixed
     age: u8,
     grade: f32,
 }
 
-pub struct StudentList {
-    students: Vec<Student>,
-}
 
-
-/// Needed for use Student and StudentList in print!()
+/// Needed for use Student in print!()
 impl fmt::Display for Student {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "Student {}\n\tAge: {}\n\tGrade: {}\n", self.name, self.age, self.grade)
     }
 }
 
-impl fmt::Display for StudentList {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let mut s = "- Student list -\n".to_string();
-        for i in self.students.iter() {
-            s.push_str(&i.to_string());
-        }
-        write!(f, "{}", s)
-    }
-}
-
+/// List/Vec of Student structure
+pub struct StudentList(Vec<Student>);
 
 impl StudentList {
     /// Create a new empty StudentList
+    ///
+    /// For this TAD, this returns a useless vector. \
+    /// You cannot add a student without create with StudentList::with_capacity()
     ///
     /// # Examples
     /// ```
     /// let l = StudentList::new();
     /// ```
     pub fn new() -> StudentList {
-        StudentList{
-            students: Vec::<Student>::new()
-        }
+        StudentList { 0: vec![]}
     }
-    
+
     /// Create a new StudentList with capacity number_of_items
-    /// For t
-    /// 
+    ///
+    /// For this TAD, you cannot add a student if l.len() >= l.with_capacity
+    ///
     /// # Examples
     /// ```
     /// let l = StudentList::with_capacity(8);
     /// assert_eq!(l.capacity, 8);
     /// ```
     pub fn with_capacity(number_of_items: u8) -> StudentList {
-        StudentList {
-            students: Vec::<Student>::with_capacity(usize::from(number_of_items))
+        StudentList { 0: Vec::<Student>::with_capacity(usize::from(number_of_items)) }
+    }
+
+    /// Add a new Student to StudentList only if StudentList.len() < StudentList.capacity()
+    ///
+    /// # Examples
+    /// ```
+    /// let l = StudentList::with_capacity(2);
+    /// assert_eq!(l.capacity, 2);
+    /// ```
+    ///
+    pub fn add(&mut self, name: ArrayString::<U30>, age: u8, grade: f32) -> Result<&Student, &str> {
+        if &self.0.len() >= &self.0.capacity() {
+            return Err("Limite de usuários excedido");
+        }
+        match self.get_by_name(name) {
+             Some(_) => Err("Nome já existe na coleção"),
+             None => {
+                 self.0.push(
+                     Student { name, age, grade });
+                 Ok(self.0.last().unwrap())
+             }
         }
     }
-    pub fn add(&mut self, name: String, age: u8, grade: f32) -> Result<&Student, &str> {
-        match self.get_by_name(name.as_str()) {
-            Some(_) => Err("Nome já existe na coleção"),
-            None => {
-                self.students.push(Student{name, age, grade});
-                Ok(self.students.last().unwrap())
-            }
-        }
-    }
-    pub fn get_by_name(&self, name: &str) -> Option<&Student> {
-        for i in self.students.iter() {
-            if i.name.as_str() == name {
+    
+    pub fn get_by_name(&self, name: ArrayString<U30>) -> Option<&Student> {
+        for i in self.0.iter() {
+            if i.name == name {
                 return Some(i);
             }
         }
         None
     }
     pub fn get_by_age(&self, age: u8) -> Option<&Student> {
-        for i in self.students.iter() {
+        for i in self.0.iter() {
             if i.age == age {
                 return Some(i);
             }
@@ -83,7 +88,7 @@ impl StudentList {
         None
     }
     pub fn get_by_grade(&self, grade: f32) -> Option<&Student> {
-        for i in self.students.iter() {
+        for i in self.0.iter() {
             if i.grade == grade {
                 return Some(i);
             }
@@ -91,28 +96,28 @@ impl StudentList {
         None
     }
     pub fn remove(&mut self, name: &str) -> Result<&str, &str> {
-        if let Some(index) = self.students.iter().position(|n| n.name == name) {
-            self.students.swap_remove(index);
+        if let Some(index) = self.0.iter().position(|n| n.name.as_str() == name) {
+            self.0.swap_remove(index);
             return Ok("Removido com sucesso");
         }
         Err("Aluno não encontrado")
     }
 
     pub fn capacity(&mut self) -> usize {
-        self.students.capacity()
+        self.0.capacity()
     }
 
     pub fn len(&mut self) -> usize {
-        self.students.len()
+        self.0.len()
     }
 
     pub fn clear(&mut self) {
-        self.students.clear()
+        self.0.clear()
     }
 
     pub fn status(&self) -> String {
-        let size :usize = self.students.len();
-        format!("Atualmente alocado: {}, Máximo: {}", size, self.students.capacity())
+        let size :usize = self.0.len();
+        format!("Atualmente alocado: {}, Máximo: {}", size, self.0.capacity())
     }
 
 }
