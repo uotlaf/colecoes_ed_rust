438c043321c95fefbfda29fce3d53a382de6d209
uotlaf <git@uotlaf.com>
2024-04-07 15:56:46 -0300

    Use new student.rs in main.rs
    - Update case 2
    - Use separated variables for temp name, age and grade
    -

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-/*
-    X Crie uma coleção
-    X Insira elementos na coleção
-    X Liste os elementos da coleção
-    X Consulte um dos elementos da coleção
-    X Remova um elemento da função
-    Esvazie a coleção
-
-    typedef struct {
-        char nome[30];
-        int idade;
-        float nota;
-    } ALUNO;
- */
-use std::io;
-use crate::student::StudentList;
+use std::io::stdin;
+use crate::student::{StudentList};
 
 mod student;
+
+// Macro to clean input and read line from keyboard
+macro_rules! read_line {
+    ($i:expr) => {
+        $i.clear();
+        stdin().read_line(&mut $i).expect("Falha ao ler a string");
+    };
+}
+
 
 fn main() {
     let mut input : String = String::new();
     let mut student_list :StudentList = StudentList::new();
 
+    // Valores temporários utilizados nas caixas de seleção do menu
+    let mut name : student::Name;
+    let mut age : student::Age;
+    let mut grade : student::Grade;
+
+
+
     'main: loop {
-        println!("{}", student_list.status());
+        println!("Atualmente alocado: {}. Tamanho máximo: {}", student_list.len(), student_list.capacity());
         println!("\x1b[2J\x1b[HPergunta:
         1 - Criar uma coleção
         2 - Inserir elementos na coleção
@@ -35,8 +35,9 @@ fn main() {
         7 - Destruir a coleção
         8 - Sair
         ");
-        input.clear();
-        io::stdin().read_line(&mut input).expect("Falha ao ler a string");
+
+        // Limpa a input pra entrada no próximo read_line()
+        read_line!(input);
 
         match input.trim().parse::<u8>() {
             Ok(1) => {
@@ -44,8 +45,7 @@ fn main() {
                 // Pergunta a quantidade de caracteres
                 'quant: loop {
                     println!("Digite a quantidade de itens para pré-alocar");
-                    input.clear();
-                    io::stdin().read_line(&mut input).expect("Falha ao ler a string");
+                    read_line!(input);
                     match input.trim().parse::<u8>() {
                         Ok(num) => {
                             student_list = StudentList::with_capacity(num);
@@ -59,7 +59,57 @@ fn main() {
             },
             Ok(2) => {
                 // Inserir elementos na coleção
+                'data: loop {
+                    println!("Digite o nome do indivíduo");
+                    read_line!(input);
 
+                    match student::Name::try_from_str(input.clone()) {
+                        Ok(result) => {
+                            name = result;
+                            break 'data;
+                        }
+                        Err(_) => {
+                            println!("Nome muito grande. O limite é 30 caracteres");
+                        }
+                    }
+                }
+
+                'data: loop {
+                    println!("Digite a idade do indivíduo");
+                    read_line!(input);
+
+                    match input.trim().parse::<student::Age>() {
+                        Ok(result) => {
+                            age = result;
+                            break 'data;
+                        }
+                        Err(_) => {
+                            println!("Verifique a idade digitada");
+                        }
+                    }
+                }
+
+                'data: loop {
+                    println!("Digite a nota do indivíduo");
+                    read_line!(input);
+
+                    match input.trim().parse::<student::Grade>() {
+                        Ok(result) => {
+                            grade = result;
+                            break 'data;
+                        }
+                        Err(_) => {
+                            println!("Verifique a nota digitada");
+                        }
+                    }
+                }
+
+                match student_list.add(name, age, grade) {
+                    Ok(_) => {}
+                    Err(err) => {
+                        println!("{}", err);
+                    }
+                };
             },
             Ok(8) => {
                 break 'main;
